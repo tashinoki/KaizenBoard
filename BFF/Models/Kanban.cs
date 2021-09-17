622fa7b7a823a6ccc48c6054e0596c2a30981efe
tashinoki <kenya.112163@gmail.com>
2021-09-17 23:59:05 +0900

    crate model folder

---
+++ BFF/Models/Kanban.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.BFF
+{
+    public class Kanban
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ KaizenBoardUi/Models/Kanban.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KaizenBoardUi.Models
+{
+    public class Kanban
+    {
+        public Guid Id { get; set; }
+    }
+}
