290379ae6ab9c2f2009afaf487ba7a32989d6ed7
tashinoki <kenya.112163@gmail.com>
2021-09-29 23:41:36 +0900

    model move to contract

---
+++ Contract/Entitys/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contract.Models
+{
+    public class Board
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
